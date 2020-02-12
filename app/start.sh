584416015d9d3b028fa8bcd3de7ef7a831ad686e
Pablo Torregrosa <pablo.torregrosa@redbull.com>
2020-02-12 08:42:43 +0100

    Kill previous instances before spawning a new chrome instance

--- app/start.sh
+++ app/start.sh
@@ -18,18 +18,15 @@ sed -i -e 's/console/anybody/g' /etc/X11/Xwrapper.config
 useradd chromium -m -s /bin/bash -G root
 usermod -a -G root,tty chromium
 
-# adding script to start chromium
-echo "#!/bin/bash" > /home/chromium/xstart.sh
-echo "chromium-browser --kiosk $URL_LAUNCHER_URL --disable-gpu --disable-software-rasterizer --disable-dev-shm-usage --window-size=1920,1080 --start-fullscreen --incognito --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI" >> /home/chromium/xstart.sh
-chmod 770 /home/chromium/xstart.sh
-chown chromium:chromium /home/chromium/xstart.sh
-
+# adding scripts to start chromium
 echo "#!/bin/bash" > /home/chromium/dashboard_a.sh
+echo "kill $(ps -e | grep Xorg | awk '{print $1}')" >> /home/chromium/dashboard_a.sh
 echo "chromium-browser --kiosk $DASHBOARD_A --disable-gpu --disable-software-rasterizer --disable-dev-shm-usage --window-size=1920,1080 --start-fullscreen --incognito --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI" >> /home/chromium/dashboard_a.sh
 chmod 770 /home/chromium/dashboard_a.sh
 chown chromium:chromium /home/chromium/dashboard_a.sh
 
 echo "#!/bin/bash" > /home/chromium/dashboard_b.sh
+echo "kill $(ps -e | grep Xorg | awk '{print $1}')" >> /home/chromium/dashboard_b.sh
 echo "chromium-browser --kiosk $DASHBOARD_B --disable-gpu --disable-software-rasterizer --disable-dev-shm-usage --window-size=1920,1080 --start-fullscreen --incognito --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI" >> /home/chromium/dashboard_b.sh
 chmod 770 /home/chromium/dashboard_b.sh
 chown chromium:chromium /home/chromium/dashboard_b.sh
@@ -37,5 +34,6 @@ chown chromium:chromium /home/chromium/dashboard_b.sh
 
 
 # starting chromium as chrome user
+# Not needed anymore as it will be launched from the crontab
 #su -c 'startx /home/chromium/xstart.sh' chromium
 
